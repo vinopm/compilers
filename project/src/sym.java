9bfebbb56969d5c788902967077df4e602272982
amankhoza <amankhoza@hotmail.co.uk>
2015-02-25 12:16:19 +0000

    fixed entire shift/reduce problems

--- project/src/sym.java
+++ project/src/sym.java
@@ -6,16 +6,16 @@
 /** CUP generated class containing symbol constants. */
 public class sym {
   /* terminals */
-  public static final int FORALL = 54;
-  public static final int SEQ = 49;
+  public static final int FORALL = 53;
+  public static final int SEQ = 48;
   public static final int MTHAN = 16;
   public static final int RAT_CONSTANT = 56;
   public static final int LBRACKET = 11;
   public static final int EQUALITY = 26;
-  public static final int CHAR = 46;
+  public static final int CHAR = 45;
   public static final int ASTERISK = 6;
   public static final int BOOL_CONSTANT = 59;
-  public static final int INT = 42;
+  public static final int INT = 41;
   public static final int FSLASH = 7;
   public static final int CHAR_CONSTANT = 58;
   public static final int MINUS = 4;
@@ -23,32 +23,32 @@ public class sym {
   public static final int IMPLICATION = 19;
   public static final int SEMICOLON = 21;
   public static final int LTHAN = 15;
-  public static final int IN = 40;
+  public static final int IN = 39;
   public static final int TDEF = 28;
-  public static final int BOOL = 41;
+  public static final int BOOL = 40;
   public static final int COMMA = 25;
   public static final int FI = 36;
-  public static final int RAT = 44;
+  public static final int RAT = 43;
   public static final int PIPEPIPE = 17;
   public static final int PLUS = 5;
-  public static final int MAIN = 52;
+  public static final int MAIN = 51;
   public static final int IF = 34;
   public static final int EXCLAMATION = 2;
   public static final int DOT = 24;
+  public static final int NEGATIVESIGN = 54;
   public static final int EOF = 0;
-  public static final int OD = 39;
+  public static final int OD = 38;
   public static final int RETURN = 31;
   public static final int RBRACKET = 12;
   public static final int error = 1;
   public static final int EXCLAMATIONEQUALITY = 20;
   public static final int FDEF = 29;
-  public static final int TOP = 47;
+  public static final int TOP = 46;
   public static final int COLONCOLON = 13;
   public static final int EQUALITYEQUALITY = 27;
-  public static final int FOREACH = 37;
   public static final int RSQUAREBRACKET = 10;
   public static final int COLON = 14;
-  public static final int DICT = 51;
+  public static final int DICT = 50;
   public static final int ELSE = 35;
   public static final int LBRACE = 22;
   public static final int STRING_CONSTANT = 55;
@@ -57,15 +57,15 @@ public class sym {
   public static final int WHILE = 30;
   public static final int FLOAT_CONSTANT = 60;
   public static final int LTHANEQUALITY = 18;
-  public static final int FLOAT = 45;
-  public static final int THEN = 50;
+  public static final int FLOAT = 44;
+  public static final int THEN = 49;
   public static final int RBRACE = 23;
-  public static final int LEN = 48;
-  public static final int STRING = 43;
-  public static final int ALIAS = 53;
+  public static final int LEN = 47;
+  public static final int STRING = 42;
+  public static final int ALIAS = 52;
   public static final int INT_CONSTANT = 61;
   public static final int AMPERSANDAMPERSAND = 3;
-  public static final int DO = 38;
+  public static final int DO = 37;
   public static final int PRINT = 33;
   public static final int IDENTIFIER = 57;
   public static final String[] terminalNames = new String[] {
@@ -106,7 +106,6 @@ public class sym {
   "IF",
   "ELSE",
   "FI",
-  "FOREACH",
   "DO",
   "OD",
   "IN",
@@ -124,6 +123,7 @@ public class sym {
   "MAIN",
   "ALIAS",
   "FORALL",
+  "NEGATIVESIGN",
   "STRING_CONSTANT",
   "RAT_CONSTANT",
   "IDENTIFIER",
